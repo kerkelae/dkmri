140ca4104c9ee3fad2d61dddb7e98e4a82c28621
kerkelae <leevi.kerkela@protonmail.com>
2022-01-19 14:53:48 +0000

    Added functions for converting parameter arrays to tensors

--- dkmri.py
+++ dkmri.py
@@ -66,3 +66,129 @@ def _design_matrix(bvals, bvecs):
     X[:, 20] = 2 * bvals ** 2 * bvecs[:, 1] ** 2 * bvecs[:, 0] * bvecs[:, 2]
     X[:, 21] = 2 * bvals ** 2 * bvecs[:, 2] ** 2 * bvecs[:, 0] * bvecs[:, 1]
     return X
+
+
+def _params_to_D(params):
+    """Return the diffusion tensors corresponding to a parameter array.
+
+    Parameters
+    ----------
+    params : numpy.ndarray
+        Floating-point array with shape (..., 22).
+
+    Returns
+    -------
+    numpy.ndarray
+    """
+    D = np.zeros(params.shape[0:-1] + (3, 3))
+    D[..., 0, 0] = params[..., 1]
+    D[..., 1, 1] = params[..., 2]
+    D[..., 2, 2] = params[..., 3]
+    D[..., 0, 1] = params[..., 4]
+    D[..., 1, 0] = params[..., 4]
+    D[..., 0, 2] = params[..., 5]
+    D[..., 2, 0] = params[..., 5]
+    D[..., 1, 2] = params[..., 6]
+    D[..., 2, 1] = params[..., 6]
+    return D
+
+
+def _params_to_W(params):
+    """Return the kurtosis tensors corresponding to a parameter array.
+
+    Parameters
+    ----------
+    params : numpy.ndarray
+        Floating-point array with shape (..., 22).
+
+    Returns
+    -------
+    numpy.ndarray
+    """
+    W = np.zeros(params.shape[0:-1] + (3, 3, 3, 3))
+    W[..., 0, 0, 0, 0] = params[..., 7]
+    W[..., 1, 1, 1, 1] = params[..., 8]
+    W[..., 2, 2, 2, 2] = params[..., 9]
+    W[..., 1, 0, 0, 0] = params[..., 10]
+    W[..., 0, 1, 0, 0] = params[..., 10]
+    W[..., 0, 0, 1, 0] = params[..., 10]
+    W[..., 0, 0, 0, 1] = params[..., 10]
+    W[..., 2, 0, 0, 0] = params[..., 11]
+    W[..., 0, 2, 0, 0] = params[..., 11]
+    W[..., 0, 0, 2, 0] = params[..., 11]
+    W[..., 0, 0, 0, 2] = params[..., 11]
+    W[..., 0, 1, 1, 1] = params[..., 12]
+    W[..., 1, 0, 1, 1] = params[..., 12]
+    W[..., 1, 1, 0, 1] = params[..., 12]
+    W[..., 1, 1, 1, 0] = params[..., 12]
+    W[..., 2, 1, 1, 1] = params[..., 13]
+    W[..., 1, 2, 1, 1] = params[..., 13]
+    W[..., 1, 1, 2, 1] = params[..., 13]
+    W[..., 1, 1, 1, 2] = params[..., 13]
+    W[..., 0, 2, 2, 2] = params[..., 14]
+    W[..., 2, 0, 2, 2] = params[..., 14]
+    W[..., 2, 2, 0, 2] = params[..., 14]
+    W[..., 2, 2, 2, 0] = params[..., 14]
+    W[..., 1, 2, 2, 2] = params[..., 15]
+    W[..., 2, 1, 2, 2] = params[..., 15]
+    W[..., 2, 2, 1, 2] = params[..., 15]
+    W[..., 2, 2, 2, 1] = params[..., 15]
+    W[..., 0, 0, 1, 1] = params[..., 16]
+    W[..., 0, 1, 1, 0] = params[..., 16]
+    W[..., 1, 1, 0, 0] = params[..., 16]
+    W[..., 1, 0, 0, 1] = params[..., 16]
+    W[..., 1, 0, 1, 0] = params[..., 16]
+    W[..., 0, 1, 0, 1] = params[..., 16]
+    W[..., 0, 0, 2, 2] = params[..., 17]
+    W[..., 0, 2, 2, 0] = params[..., 17]
+    W[..., 2, 2, 0, 0] = params[..., 17]
+    W[..., 2, 0, 0, 2] = params[..., 17]
+    W[..., 2, 0, 2, 0] = params[..., 17]
+    W[..., 0, 2, 0, 2] = params[..., 17]
+    W[..., 1, 1, 2, 2] = params[..., 18]
+    W[..., 1, 2, 2, 1] = params[..., 18]
+    W[..., 2, 2, 1, 1] = params[..., 18]
+    W[..., 2, 1, 1, 2] = params[..., 18]
+    W[..., 2, 1, 2, 1] = params[..., 18]
+    W[..., 1, 2, 1, 2] = params[..., 18]
+    W[..., 0, 0, 1, 2] = params[..., 19]
+    W[..., 0, 0, 2, 1] = params[..., 19]
+    W[..., 0, 1, 0, 2] = params[..., 19]
+    W[..., 0, 1, 2, 0] = params[..., 19]
+    W[..., 0, 2, 0, 1] = params[..., 19]
+    W[..., 0, 2, 1, 0] = params[..., 19]
+    W[..., 1, 0, 0, 2] = params[..., 19]
+    W[..., 1, 0, 2, 0] = params[..., 19]
+    W[..., 1, 2, 0, 0] = params[..., 19]
+    W[..., 2, 0, 0, 1] = params[..., 19]
+    W[..., 2, 0, 1, 0] = params[..., 19]
+    W[..., 2, 1, 0, 0] = params[..., 19]
+    W[..., 1, 1, 0, 2] = params[..., 20]
+    W[..., 1, 1, 2, 0] = params[..., 20]
+    W[..., 1, 0, 1, 2] = params[..., 20]
+    W[..., 1, 0, 2, 1] = params[..., 20]
+    W[..., 1, 2, 1, 0] = params[..., 20]
+    W[..., 1, 2, 0, 1] = params[..., 20]
+    W[..., 0, 1, 1, 2] = params[..., 20]
+    W[..., 0, 1, 2, 1] = params[..., 20]
+    W[..., 0, 2, 1, 1] = params[..., 20]
+    W[..., 2, 1, 1, 0] = params[..., 20]
+    W[..., 2, 1, 0, 1] = params[..., 20]
+    W[..., 2, 0, 1, 1] = params[..., 20]
+    W[..., 2, 2, 0, 1] = params[..., 21]
+    W[..., 2, 2, 1, 0] = params[..., 21]
+    W[..., 2, 0, 2, 1] = params[..., 21]
+    W[..., 2, 0, 1, 2] = params[..., 21]
+    W[..., 2, 1, 2, 0] = params[..., 21]
+    W[..., 2, 1, 0, 2] = params[..., 21]
+    W[..., 0, 2, 2, 1] = params[..., 21]
+    W[..., 0, 2, 1, 2] = params[..., 21]
+    W[..., 0, 1, 2, 2] = params[..., 21]
+    W[..., 1, 2, 2, 0] = params[..., 21]
+    W[..., 1, 2, 0, 2] = params[..., 21]
+    W[..., 1, 0, 2, 2] = params[..., 21]
+    evals, _ = np.linalg.eigh(_params_to_D(params))
+    MD = np.mean(evals, axis=-1)[..., np.newaxis, np.newaxis, np.newaxis, np.newaxis]
+    MD[MD == 0] = np.nan  # To avoid warnings due to division by zero
+    W /= MD ** 2
+    return W
