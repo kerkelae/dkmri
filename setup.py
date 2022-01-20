dc182d2a445a10859d91328bc2b2fde543693342
kerkelae <leevi.kerkela@protonmail.com>
2022-01-20 09:56:45 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="dkmri",
-    version="0.0.1",
+    version="0.0.2",
     description="Reproducible and efficient diffusion kurtosis imaging in Python.",
     url="https://github.com/kerkelae/dkmri",
     author="Leevi Kerkelä",
