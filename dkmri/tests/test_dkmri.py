53c95156a1b60a7d0a49b31b7e95aab898a2ffe6
kerkelae <leevi.kerkela@protonmail.com>
2022-01-26 16:18:42 +0000

    Made some private functions public

--- dkmri/tests/test_dkmri.py
+++ dkmri/tests/test_dkmri.py
@@ -34,7 +34,7 @@ params = np.array(
 )
 
 
-def test__design_matrix():
+def test_design_matrix():
     bvals = np.arange(5)
     bvecs = np.array(
         [
@@ -71,11 +71,11 @@ def test__design_matrix():
             [0.0, 0.0, 0.0, 0.0, 0.0],
         ]
     ).T
-    X = dkmri._design_matrix(bvals, bvecs)
+    X = dkmri.design_matrix(bvals, bvecs)
     npt.assert_almost_equal(X, desired_X)
 
 
-def test__params_to_D():
+def test_params_to_D():
     desired_D = np.array(
         [
             [0.88660664, 0.25016042, 0.12341918],
@@ -83,11 +83,11 @@ def test__params_to_D():
             [0.12341918, 0.28344717, 0.81741033],
         ]
     )
-    D = dkmri._params_to_D(params)
+    D = dkmri.params_to_D(params)
     npt.assert_almost_equal(D, desired_D)
 
 
-def test__params_to_W():
+def test_params_to_W():
     desired_W = np.array(
         [
             [
@@ -143,21 +143,21 @@ def test__params_to_W():
             ],
         ]
     )
-    W = dkmri._params_to_W(params)
+    W = dkmri.params_to_W(params)
     npt.assert_almost_equal(W, desired_W)
 
 
-def test__tensors_to_params():
+def test_tensors_to_params():
     S0 = np.exp(params[..., 0])
-    D = dkmri._params_to_D(params)
-    W = dkmri._params_to_W(params)
-    npt.assert_almost_equal(dkmri._tensors_to_params(S0, D, W), params)
+    D = dkmri.params_to_D(params)
+    W = dkmri.params_to_W(params)
+    npt.assert_almost_equal(dkmri.tensors_to_params(S0, D, W), params)
     return
 
 
 def test__adc():
     np.random.seed(SEED)
-    D = dkmri._params_to_D(params)
+    D = dkmri.params_to_D(params)
     for _ in range(100):
         v = np.random.random((1, 3)) - 0.5
         v /= np.linalg.norm(v)
@@ -170,32 +170,32 @@ def test__adc():
         npt.assert_almost_equal(adcs[1], adc)
 
 
-def test__md():
+def test_params_to_md():
     desired_md = 0.8419605533333335
-    md = dkmri._md(params)
+    md = dkmri.params_to_md(params)
     npt.assert_almost_equal(md, desired_md)
 
 
-def test__ad():
+def test_params_to_ad():
     desired_ad = 1.2839527280964818
-    ad = dkmri._ad(params)
+    ad = dkmri.params_to_ad(params)
     npt.assert_almost_equal(ad, desired_ad)
 
 
-def test__rd():
+def test_params_to_rd():
     desired_rd = 0.6209644659517595
-    rd = dkmri._rd(params)
+    rd = dkmri.params_to_rd(params)
     npt.assert_almost_equal(rd, desired_rd)
 
 
 def test__akc():
     np.random.seed(SEED)
-    D = dkmri._params_to_D(params)
-    W = dkmri._params_to_W(params)
+    D = dkmri.params_to_D(params)
+    W = dkmri.params_to_W(params)
     for _ in range(100):
         v = np.random.random((1, 3)) - 0.5
         v /= np.linalg.norm(v)
-        md = dkmri._md(params)
+        md = dkmri.params_to_md(params)
         adc = dkmri._adc(params, v)
         desired_akc = (md / adc) ** 2 * v[0] @ (v[0] @ W @ v[0]) @ v[0]
         akc = np.asarray(dkmri._akc(params, v))
@@ -206,20 +206,20 @@ def test__akc():
         npt.assert_almost_equal(akcs[1], akc)
 
 
-def test__mk():
+def test_params_to_mk():
     desired_mk = 1.1124342668323295
-    mk = dkmri._mk(params)
+    mk = dkmri.params_to_mk(params)
     npt.assert_almost_equal(mk, desired_mk)
 
 
-def test__ak():
+def test_params_to_ak():
     desired_ak = 0.7109767625600302
-    ak = dkmri._ak(params)
+    ak = dkmri.params_to_ak(params)
     npt.assert_almost_equal(ak, desired_ak)
 
 
-def test__rk():
+def test_params_to_rk():
     desired_rk = 1.5180490434619633
-    rk = dkmri._rk(params)
+    rk = dkmri.params_to_rk(params)
     npt.assert_almost_equal(rk, desired_rk)
 
