4b0941100606f9354609eec745c1a3841f42f853
kerkelae <leevi.kerkela@protonmail.com>
2022-02-04 14:16:02 +0000

    Added explicit imports for private methods

--- dkmri/__init__.py
+++ dkmri/__init__.py
@@ -4,3 +4,15 @@ Visit https://github.com/kerkelae/dkmri for more info.
 """
 
 from .dkmri import *
+from .dkmri import (
+    _adc,
+    _akc,
+    _vec2vec_rotmat,
+    _mtk,
+    _ols_fit,
+    _nlls_fit,
+    _akc_mask,
+    _predict,
+    _calculate_x0,
+    _reg_nlls_fit,
+)
