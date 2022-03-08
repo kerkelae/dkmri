b4ccab050eba50cee27c2afefd908de3bc7cdf0c
kerkelae <leevi.kerkela@protonmail.com>
2022-03-08 15:27:15 +0000

    Updated version number and init imports

--- dkmri/__init__.py
+++ dkmri/__init__.py
@@ -3,10 +3,14 @@
 Visit https://github.com/kerkelae/dkmri for more info.
 """
 
+__version__ = "0.1.0"
+
 from .dkmri import *
 from .dkmri import (
     _adc,
     _akc,
+    _45_dirs,
+    _10_dirs,
     _vec2vec_rotmat,
     _mtk,
     _ols_fit,
